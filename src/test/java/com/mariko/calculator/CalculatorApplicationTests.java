d408cb5aeb65612adb458029cfe8812e59c02051
sidiki <sidiki@debian>
2019-11-26 15:30:55 +0100

    calculator: fix on CalculatorTest.java - again ... :(

--- src/test/java/com/mariko/calculator/CalculatorApplicationTests.java
+++ src/test/java/com/mariko/calculator/CalculatorApplicationTests.java
@@ -1,13 +1,11 @@
 package com.mariko.calculator;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class CalculatorApplicationTests {
-
-	@Test
-	void contextLoads() {
+public class CalculatorTest {
+private Calculator calculator = new Calculator();
+@Test
+public void testSum() {
+	assertEquals(5, calculator.sum(2, 3));
 	}
-
 }
